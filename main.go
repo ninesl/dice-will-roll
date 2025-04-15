418372d0798863e3bbd24511b01b3773ef2f2a3e
ninesl <lance.nines@gmail.com>
2025-04-14 23:49:32 -0400

    basic spritesheet impl

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct {
+	TileSize int
+	Dice     *Sprite
+}
+
+func (g *Game) Update() error {
+
+	g.Dice.X++
+	g.Dice.Y++
+	g.Dice.SpriteSheet.Animate()
+
+	// fmt.Println("HELLO?")
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// ebitenutil.DebugPrint(screen, "Hello, World!")
+	opts := &ebiten.DrawImageOptions{}
+
+	opts.GeoM.Translate(g.Dice.X, g.Dice.Y)
+	screen.DrawImage(
+		g.Dice.Image.SubImage(
+			g.Dice.SpriteSheet.Rect(g.Dice.SpriteSheet.ActiveFrame),
+		).(*ebiten.Image),
+		opts,
+	)
+}
+
+// 320 x 240 is the pixels in the game
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return g.TileSize * 16, g.TileSize * 9
+}
+
+func loadGame() *Game {
+	diceImg, _, err := ebitenutil.NewImageFromFile("assets/images/dice.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dieImgSize := diceImg.Bounds().Dx() / 6
+
+	diceSprite := Sprite{
+		Image:       diceImg,
+		SpriteSheet: NewSpriteSheet(6, 7, dieImgSize),
+	}
+
+	return &Game{
+		TileSize: dieImgSize,
+		Dice:     &diceSprite,
+	}
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	ebiten.SetWindowSize(1600, 900) // resolution
+	ebiten.SetWindowTitle("Dice Will Roll")
+
+	if err := ebiten.RunGame(loadGame()); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ sprites.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"image"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Sprite struct {
+	Image       *ebiten.Image
+	X, Y        float64
+	SpriteSheet SpriteSheet
+}
+
+// assumed all tiles are squares
+type SpriteSheet struct {
+	WidthTiles  int
+	HeightTiles int
+	TileSize    int
+	TileAmount  int
+	ActiveFrame int
+}
+
+func NewSpriteSheet(w, h, t int) SpriteSheet {
+	return SpriteSheet{
+		WidthTiles:  w,
+		HeightTiles: h,
+		TileSize:    t,
+		TileAmount:  w * h,
+		ActiveFrame: 0,
+	}
+}
+
+// Gets the 'index' of the sheet
+func (s *SpriteSheet) Rect(index int) image.Rectangle {
+	x := (index % s.WidthTiles) * s.TileSize
+	y := (index / s.WidthTiles) * s.TileSize
+
+	return image.Rect(
+		x, y, x+s.TileSize, y+s.TileSize,
+	)
+}
+
+// updates the active frame by 1, resets back to 0
+func (s *SpriteSheet) Animate() {
+	s.ActiveFrame++
+	if s.ActiveFrame > s.TileAmount {
+		s.ActiveFrame = 0
+	}
+}
