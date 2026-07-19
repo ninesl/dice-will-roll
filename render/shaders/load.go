624cd929af1ca197b93da87f60c8886757bb0159
Lance Nines <lance.nines@gmail.com>
2026-07-19 19:03:38 -0400

    refactor shader inits

--- render/shaders/load.go
+++ render/shaders/load.go
@@ -1,8 +1,9 @@
 package shaders
 
 import (
-	_ "embed"
+	"embed"
 	"fmt"
+	"io/fs"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -17,37 +18,30 @@ var (
 	ErrShader    error = fmt.Errorf("shader could not be set")
 	ErrNilShader error = fmt.Errorf("shader could not be found")
 )
+
+//go:embed kages
+var kageShaders embed.FS
+
 var (
-	// GAME OBJECTS
-
-	//go:embed die.kage
-	dieKage []byte
-
-	// //go:embed rocks.kage
-	// rocksKage []byte
-
-	//go:embed moon_rock.kage
-	rocksKage []byte
-
-	//go:embed color_filter.kage
-	colorFilterKage []byte
-
-	// POST PROCESSING
-
-	//go:embed fxaa.kage
-	fxaaKage []byte
-
-	//go:embed background.kage
-	backgroundKage []byte
-
-	//go:embed explosion.kage
-	explosionKage []byte
-
-	//go:embed color.kage
-	colorKage []byte
+	RocksShaders = subFS(kageShaders, "kages/rocks")
+	FXShaders    = subFS(kageShaders, "kages/fx")
+	DiceShaders  = subFS(kageShaders, "kages/dice")
 )
 
-func loadShader(kageShader []byte) *ebiten.Shader {
+func subFS(shaderFS embed.FS, dir string) fs.FS {
+	categoryFS, err := fs.Sub(shaderFS, dir)
+	if err != nil {
+		panic(err)
+	}
+	return categoryFS
+}
+
+func loadShader(shaderFS fs.FS, path string) *ebiten.Shader {
+	kageShader, err := fs.ReadFile(shaderFS, path)
+	if err != nil {
+		panic(err)
+	}
+
 	shader, err := ebiten.NewShader(kageShader)
 	if err != nil {
 		panic(err)
@@ -70,13 +64,15 @@ const (
 func LoadShaders() map[ShaderKey]*ebiten.Shader {
 	var shaders = map[ShaderKey]*ebiten.Shader{}
 
-	shaders[ExplosionShaderKey] = loadShader(explosionKage)
-	shaders[DieShaderKey] = loadShader(dieKage)
-	shaders[RocksShaderKey] = loadShader(rocksKage)
-	shaders[ColorFilterShaderKey] = loadShader(colorFilterKage)
-	shaders[FXAAShaderKey] = loadShader(fxaaKage)
-	shaders[BackgroundShaderKey] = loadShader(backgroundKage)
-	shaders[ColorShaderKey] = loadShader(colorKage)
+	shaders[ExplosionShaderKey] = loadShader(RocksShaders, "explosion.kage")
+	shaders[RocksShaderKey] = loadShader(RocksShaders, "moon_rock.kage")
+
+	shaders[DieShaderKey] = loadShader(DiceShaders, "die.kage")
+
+	shaders[ColorFilterShaderKey] = loadShader(FXShaders, "color_filter.kage")
+	shaders[FXAAShaderKey] = loadShader(FXShaders, "fxaa.kage")
+	shaders[BackgroundShaderKey] = loadShader(FXShaders, "background.kage")
+	shaders[ColorShaderKey] = loadShader(FXShaders, "color.kage")
 
 	return shaders
 }
